b05d9f4ccb3cf391210c3340556e362cad73089d
Paulo Andreola <pauloandreola@gmail.com>
2023-04-19 20:03:30 -0300

    fix: export schema

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,7 +1,7 @@
 import { Schema } from 'mongoose'
 import { IUser } from '../dtos/IUserDTO'
 
-const userSchema = new Schema<IUser>({
+export const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, lowercase: true, unique: true },
   password: { type: String, required: true },
