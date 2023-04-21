75949ba74c6b9fbc8d63c5f719b107729e7204c2
Paulo Andreola <pauloandreola@gmail.com>
2023-04-21 11:02:53 -0300

    feat: user create method with controller and use case

---
+++ src/modules/useCases/user/createUser/CreateUserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { container } from 'tsyringe'
+
+import { CreateUserUseCase } from './CreateUserUseCase'
+
+export class CreateUserController {
+  async handle (req: Request, res: Response): Promise<Response> {
+    const { name, email, password, confirmPassword } = req.body
+    try {
+      const createUserUseCase = container.resolve(CreateUserUseCase)
+      const user = await createUserUseCase.execute(name, email, password, confirmPassword)
+
+      return res.status(201).json(user)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  }
+}
---
+++ src/modules/useCases/user/createUser/CreateUserUseCase.ts
@@ -0,0 +1,35 @@
+import { inject, injectable } from 'tsyringe'
+import { hash } from 'bcryptjs'
+
+import { IUserRepository } from '../../../repositories/user/IUserRepository'
+
+@injectable()
+export class CreateUserUseCase {
+  constructor (@inject('UserRepository')private userRepository: IUserRepository
+  ) {}
+
+  async execute (name: string, email: string, password: string, confirmPassword: string): Promise<void> {
+    const userVerificationExist = await this.userRepository.findUserByEmail(email)
+    if (!name) {
+      throw new Error('Please check the name field blank!')
+    }
+
+    if (!email) {
+      throw new Error('Please check the e-mail field blank!')
+    }
+
+    if (!password) {
+      throw new Error('Please check the password field blank!')
+    }
+
+    if (password !== confirmPassword) {
+      throw new Error('Password is not the same!')
+    }
+    if (userVerificationExist) {
+      throw new Error('User Already Exists!')
+    }
+
+    const passwordHash = await hash(password, 8)
+    this.userRepository.createUser(name, email, passwordHash)
+  }
+}
