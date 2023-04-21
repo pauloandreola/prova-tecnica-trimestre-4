d0a37f279fa40360d7ff870d342a7c7e811a0af8
Paulo Andreola <pauloandreola@gmail.com>
2023-04-21 14:10:00 -0300

    feat:update user routes with login user method

--- src/shared/routes/user.routes.ts
+++ src/shared/routes/user.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 
 import { CreateUserController } from '../../modules/useCases/user/createUser/CreateUserController'
+import { LoginUserController } from '../../modules/useCases/user/loginUser/LoginUserController'
 
 export const routerUser = Router()
 
 const createUserController = new CreateUserController()
+const loginUserController = new LoginUserController()
 
 routerUser.post('/', createUserController.handle)
+routerUser.post('/login', loginUserController.handle)
