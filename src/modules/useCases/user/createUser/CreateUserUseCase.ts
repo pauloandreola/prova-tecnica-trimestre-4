920272ec823b2dd292734474e4f1a63fcf957e85
Paulo Andreola <pauloandreola@gmail.com>
2023-04-21 14:48:07 -0300

    feat: clean code create user use case file

--- src/modules/useCases/user/createUser/CreateUserUseCase.ts
+++ src/modules/useCases/user/createUser/CreateUserUseCase.ts
@@ -5,23 +5,19 @@ import { IUserRepository } from '../../../repositories/users/IUserRepository'
 
 @injectable()
 export class CreateUserUseCase {
-  constructor (@inject('UserRepository')private userRepository: IUserRepository
-  ) {}
+  constructor (@inject('UserRepository')private userRepository: IUserRepository) {}
 
   async execute (name: string, email: string, password: string, confirmPassword: string): Promise<void> {
     const userVerificationExist = await this.userRepository.findUserByEmail(email)
     if (!name) {
       throw new Error('Please check the name field blank!')
     }
-
     if (!email) {
       throw new Error('Please check the e-mail field blank!')
     }
-
     if (!password) {
       throw new Error('Please check the password field blank!')
     }
-
     if (password !== confirmPassword) {
       throw new Error('Password is not the same!')
     }
