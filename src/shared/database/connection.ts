b8bff7aa1b7f12a460f7554bee11c010966e69a7
Paulo Andreola <pauloandreola@gmail.com>
2023-04-21 10:18:18 -0300

    refactor: update dotenv and connection method, include db class from mongodb and remove mongoose lib

--- src/shared/database/connection.ts
+++ src/shared/database/connection.ts
@@ -1,13 +1,19 @@
-import mongoose from 'mongoose'
-import * as dotenv from 'dotenv'
+import { Db, MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
 
 dotenv.config()
 
-export async function connectMongoDB () {
-  try {
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mn8tn9c.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mn8tn9c.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
-    console.log('MongoDB Altas connected!')
+export async function connectMongoDB (): Promise<Db> {
+  try {
+    const client = new MongoClient(uri)
+    await client.connect()
+
+    console.log('MongoDB Atlas connected!')
+
+    const database: Db = client.db(process.env.DB_NAME)
+    return database
   } catch (err) {
     console.log('Not connected at MongoDB Atlas verify above =>', err)
   }
