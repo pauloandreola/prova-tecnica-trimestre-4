3ecc9ae386d92ae9931135e20883e000fbda8e51
Paulo Andreola <pauloandreola@gmail.com>
2023-04-21 15:14:40 -0300

    feat:update user routes with refresh token method

--- src/shared/routes/user.routes.ts
+++ src/shared/routes/user.routes.ts
@@ -2,11 +2,14 @@ import { Router } from 'express'
 
 import { CreateUserController } from '../../modules/useCases/user/createUser/CreateUserController'
 import { LoginUserController } from '../../modules/useCases/user/loginUser/LoginUserController'
+import { RefreshTokenUserController } from '../../modules/useCases/user/refreshTokenUser/RefreshTokenUserController'
 
 export const routerUser = Router()
 
 const createUserController = new CreateUserController()
 const loginUserController = new LoginUserController()
+const refreshTokenUserController = new RefreshTokenUserController()
 
 routerUser.post('/', createUserController.handle)
 routerUser.post('/login', loginUserController.handle)
+routerUser.post('/refresh', refreshTokenUserController.handle)
