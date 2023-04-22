d491e081ce17c5d1c0dd23dd12e15774702a2f2d
Paulo Andreola <pauloandreola@gmail.com>
2023-04-22 01:07:33 -0300

    feat: update singleton file to task repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,8 @@
 import { container } from 'tsyringe'
+import { ITasksRepository } from '../../modules/repositories/task/ITasksRepository'
+import { TasksRepository } from '../../modules/repositories/task/TasksRepository'
+import { IUsersRepository } from '../../modules/repositories/user/IUsersRepository'
+import { UsersRepository } from '../../modules/repositories/user/UsersRepository'
 
-import { IUserRepository } from '../../modules/repositories/users/IUserRepository'
-import { UserRepository } from '../../modules/repositories/users/UserRepository'
-
-container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
+container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository)
+container.registerSingleton<ITasksRepository>('TasksRepository', TasksRepository)
