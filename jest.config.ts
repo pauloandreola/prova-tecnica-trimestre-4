3cb72ccc43f4d6a91ba1fbd9d196ac51e0f1bafa
Paulo Andreola <pauloandreola@gmail.com>
2023-04-24 15:42:11 -0300

    feat: jest config file

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['text-summary', 'lcov'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts']
+}
