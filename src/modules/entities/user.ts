5faa7d582d841f5979f8e847291a72aa62e8615e
Paulo Andreola <pauloandreola@gmail.com>
2023-04-24 15:32:30 -0300

    fix: update refresh method

--- src/modules/entities/user.ts
+++ src/modules/entities/user.ts
@@ -1,11 +1,13 @@
+import { RefreshArray } from '../repositories/user/UsersRepository'
+
 export class User {
   _id!: string
   name: string
   email: string
   password: string
-  refreshToken?: string
+  refreshToken: RefreshArray[]
 
-  constructor (_id: string, name: string, email: string, password: string, refreshToken: string) {
+  constructor (_id: string, name: string, email: string, password: string, refreshToken: RefreshArray[]) {
     this._id = _id
     this.name = name
     this.email = email
