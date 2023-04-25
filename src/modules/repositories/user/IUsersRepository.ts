e02de9f965da2d862044d057cd7513cce3b7b2ea
Paulo Andreola <pauloandreola@gmail.com>
2023-04-25 13:41:21 -0300

    refactor: update name file

--- src/modules/repositories/user/IUsersRepository.ts
+++ src/modules/repositories/user/IUsersRepository.ts
@@ -1,8 +1,8 @@
-import { User } from '../../entities/User'
+import { User } from '../../entities/user'
 
 export interface IUsersRepository {
-  createUser (name: string, email: string, password: string): Promise<void>;
+  createUser (name: string, email: string, password: string): Promise<User>;
   findUserByEmail (email: string): Promise<User>;
   findUserById (userId: string): Promise<User>;
-  updateRefreshToken (userId: string, refreshToken: string): Promise<void>;
+  updateRefreshToken (userId: string, refreshToken: string, createdAt: Date): Promise<void>;
 }
