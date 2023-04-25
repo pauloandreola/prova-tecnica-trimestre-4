55b72aad46dc05ea5766e6f961a86fee285de50b
Paulo Andreola <pauloandreola@gmail.com>
2023-04-25 09:20:54 -0300

    feat: organized code and create new tests

--- src/modules/useCases/user/createUser/CreateUserUseCase.spec.ts
+++ src/modules/useCases/user/createUser/CreateUserUseCase.spec.ts
@@ -6,7 +6,7 @@ import { UsersRepositoryInMemory } from '../../../../modules/repositories/user/i
 let createUserUseCase: CreateUserUseCase
 let usersRepositoryInMemory: UsersRepositoryInMemory
 
-describe('Create a User', () => {
+describe('Create an User', () => {
   beforeAll(async () => {
     usersRepositoryInMemory = new UsersRepositoryInMemory()
     createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory)
@@ -15,12 +15,30 @@ describe('Create a User', () => {
   it('should be able to create a new user', async () => {
     await createUserUseCase.execute('Paulo01', 'paulo01@gmail.com', '1234', '1234')
 
-    const user = await usersRepositoryInMemory.findUserByEmail(
-      'paulo01@gmail.com'
-    )
+    const user = await usersRepositoryInMemory.findUserByEmail('paulo01@gmail.com')
 
     expect(user.name).toBe('Paulo01')
     expect(user.email).toBe('paulo01@gmail.com')
+  })
+
+  it('should not be able to create new user without name', async () => {
+    await expect(createUserUseCase.execute('', 'paulo01@gmail.com', '1234', '1234')
+    ).rejects.toEqual(new Error('Please check the name field blank!'))
+  })
+
+  it('should not be able to create new user without email', async () => {
+    await expect(createUserUseCase.execute('Paulo02', '', '1234', '1234')
+    ).rejects.toEqual(new Error('Please check the e-mail field blank!'))
+  })
+
+  it('should not be able to create new user without password', async () => {
+    await expect(createUserUseCase.execute('Paulo02', 'paulo01@gmail.com', '', '1234')
+    ).rejects.toEqual(new Error('Please check the password field blank!'))
+  })
+
+  it('should not be able to create new user with not the same password and confirm password', async () => {
+    await expect(createUserUseCase.execute('Paulo02', 'paulo01@gmail.com', '12345', '1234')
+    ).rejects.toEqual(new Error('Password is not the same!'))
   })
 
   it('should not be able to create new user with same email', async () => {
