4245baf52663380493bd03aa2de7e0fd68585f80
Paulo Andreola <pauloandreola@gmail.com>
2023-04-27 09:17:34 -0300

    refactor: update and optimized methods

--- src/modules/repositories/task/TasksRepository.ts
+++ src/modules/repositories/task/TasksRepository.ts
@@ -5,9 +5,9 @@ import { Task } from '../../entities/Task'
 import { ITasksRepository } from './ITasksRepository'
 
 export class TasksRepository implements ITasksRepository {
-  async createTask (userId: string, title: string, description: string, done: boolean): Promise<Task> {
+  async createTask (userId: string, title: string, description: string, done: boolean, createdAt: Date): Promise<Task> {
     const database = await connectMongoDB()
-    const task = await database.collection('tasks').insertOne({ userId, title, description, done })
+    const task = await database.collection('tasks').insertOne({ userId, title, description, done, createdAt })
     return task as any
   }
 
@@ -24,22 +24,17 @@ export class TasksRepository implements ITasksRepository {
 
   async findTasksByTitleAndDone (userId: string, title: string, done: boolean): Promise<Task[]> {
     const database = await connectMongoDB()
-    const query = { userId }
-    if (done !== undefined) {
-      query['done'] = { $eq: (done === false || done === true) }
-    }
-
-    if (title) {
-      query['title'] = { $regex: title, $options: 'i' }
-    }
-
-    const tasks = await database.collection('tasks').find(query).toArray()
-    return tasks as unknown as Task[]
+    const query = await database.collection('tasks').find({
+      $and: [{ userId: { $regex: userId, $options: 'i' } }, { title: { $regex: title, $options: 'i' } },
+        { done: { $eq: done } }]
+    }).toArray()
+    return query as unknown as Task[]
   }
 
-  async updatedTask (taskId: string, title: string, description: string, done: boolean): Promise<void> {
+  async updatedTask (taskId: string, title: string, description: string, done: boolean, updateAt: Date): Promise<void> {
     const database = await connectMongoDB()
-    const taskUpdated = await database.collection('tasks').findOneAndUpdate({ _id: new ObjectId(taskId) }, { $set: { title, description, done } })
+    const taskUpdated = await database.collection('tasks').findOneAndUpdate({ _id: new ObjectId(taskId) },
+      { $set: { title, description, done, updateAt } })
     return taskUpdated as any
   }
 }
