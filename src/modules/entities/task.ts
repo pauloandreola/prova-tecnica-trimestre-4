b541df7a448f7b9f8c67cb63cd1c7d6efe80c6c4
Paulo Andreola <pauloandreola@gmail.com>
2023-04-27 07:43:16 -0300

    feat: insert createdat and updatedat vairables

--- src/modules/entities/task.ts
+++ src/modules/entities/task.ts
@@ -1,14 +1,20 @@
+import { Date } from 'mongoose'
+
 export class Task {
   _id!: string
   userId: string
   title: string
   description: string
   done: boolean
+  createdAt?: Date
+  updateAt?: Date
 
-  constructor (userId: string, title: string, description: string, done: boolean) {
+  constructor (userId: string, title: string, description: string, done: boolean, createdAt: Date, updateAt: Date) {
     this.userId = userId
     this.title = title
     this.description = description
     this.done = done
+    this.createdAt = createdAt
+    this.updateAt = updateAt
   }
 }
