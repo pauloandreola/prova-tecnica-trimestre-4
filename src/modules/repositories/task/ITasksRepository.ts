1ab91ae62f03b8b4d144aeae353b0bafcf24a773
Paulo Andreola <pauloandreola@gmail.com>
2023-04-27 09:13:12 -0300

    feat: insert createdat in createtask and updatedat in updatetask interfaces

--- src/modules/repositories/task/ITasksRepository.ts
+++ src/modules/repositories/task/ITasksRepository.ts
@@ -1,10 +1,10 @@
 import { Task } from '../../entities/Task'
 
 export interface ITasksRepository {
-  createTask (userId: string, title: string, description: string, done: boolean): Promise<Task>
+  createTask (userId: string, title: string, description: string, done: boolean, createdAt: Date): Promise<Task>
   deleteTaskById (taskId: string): Promise<void>
   findTaskById (taskId: string): Promise<Task>
   findTasksByTitleAndDone (userId: string, title: string, done: boolean): Promise<Task[]>
-  updatedTask (taskId: string, title: string, description: string, done: boolean): Promise<void>
+  updatedTask (taskId: string, title: string, description: string, done: boolean, updateAt: Date): Promise<void>
 
 }
