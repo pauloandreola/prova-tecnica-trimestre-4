971566315ffa78369fef5c28622eef5c119bab02
Paulo Andreola <pauloandreola@gmail.com>
2023-04-27 07:18:33 -0300

    refactor: update variable names and organized code

--- src/shared/middlewares/authenticateToken.ts
+++ src/shared/middlewares/authenticateToken.ts
@@ -6,19 +6,20 @@ import { connectMongoDB } from '../database/connection'
 
 dotenv.config()
 
-const secret = process.env.TOKEN
+const secret = process.env.REFRESHTOKEN
 
 export async function authenticateToken (req: Request, res: Response, next: NextFunction) {
   const authenticateToken = req.headers.authorization
-  const token = authenticateToken?.split(' ')[1]
-  if (!token) {
+  const refreshToken = authenticateToken?.split(' ')[1]
+  if (!refreshToken) {
     return res.status(404).json('Token not found!')
   }
 
   try {
+    const data = await verify(refreshToken, secret) as { userId: string}
+    const verifyUser = data.userId
     const database = await connectMongoDB()
-    const data = verify(token, secret) as { email: string}
-    const user = await database.collection('tasks').findOne({ data })
+    const user = database.collection('tasks').findOne({ verifyUser })
     if (!user) {
       return res.status(401).json('Invalid user!')
     }
